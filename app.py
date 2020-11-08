a64d5ae7a436e4024a6a1974765024111c570c5e
Garcia Lu <gargar070402@gmail.com>
2020-11-09 07:39:09 +0800

    Initial commit

--- app.py
+++ app.py
@@ -3,27 +3,27 @@ import os
 from flask import Flask
 
 app = Flask(__name__, instance_relative_config=True)
-def create_app(test_config=None):
-    # create and configure the app
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
-    )
+# def create_app(test_config=None):
+#     # create and configure the app
+#     app.config.from_mapping(
+#         SECRET_KEY='dev',
+#         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+#     )
 
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
+#     if test_config is None:
+#         # load the instance config, if it exists, when not testing
+#         app.config.from_pyfile('config.py', silent=True)
+#     else:
+#         # load the test config if passed in
+#         app.config.from_mapping(test_config)
 
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
+#     # ensure the instance folder exists
+#     try:
+#         os.makedirs(app.instance_path)
+#     except OSError:
+#         pass
 
-    return app
+#     return app
 
 # a simple page that says hello
 @app.route('/')
@@ -37,5 +37,5 @@ def redirect(short_url):
     return short_url
 
 if __name__ == '__main__':
-    create_app()
+    # create_app()
     app.run()
